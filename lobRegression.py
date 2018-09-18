e22c559e1a6eac3881ecf3d0c6447525733370a3
ChetWang <elwinwang1993@gmail.com>
2018-09-18 17:42:37 +0800

    update back-testing logic

--- lobRegression.py
+++ lobRegression.py
@@ -2,13 +2,22 @@
 
 import pandas as pd
 import numpy as np
+import sys
+from datetime import datetime
 from sklearn import linear_model
-data = pd.read_csv("btc-depth-sample.csv",header = 0)
+
+filename = sys.argv[1]
+roundtime = int(sys.argv[2])
+data = pd.read_csv(filename, header = 0)
+data['time'] = pd.Series(datetime.fromtimestamp(x) for x in data['timestamp'])
+data['time'] = pd.Series(x.replace(second = x.second/roundtime *roundtime, microsecond=0) for x in data['time'])
+data = data.set_index('time')
+data = data.groupby(level=0).first()
 
 VB = []
 VA = []
-#weight = np.array([10/45.0,9/45.0,8/45.0,7/45.0,6/45.0,5/45.0,4/45.0,3/45.0,2/45.0,1/45.0]).T
-weight = np.array([1.0,0,0,0,0,0,0,0,0,0]).T
+weight = np.array([9/45.0,9/45.0,9/45.0,7/45.0,6/45.0,5/45.0,4/45.0,3/45.0,2/45.0,1/45.0]).T
+#weight = np.array([1.0,0,0,0,0,0,0,0,0,0]).T
 
 for i in range(1,len(data)):
     if data['bp1'][i]<data['bp1'][i-1]:
@@ -28,24 +37,6 @@ for i in range(1,len(data)):
         VA.append(((np.array(data[['av1','av2','av3','av4','av5','av6','av7','av8','av9','av10']][i:i + 1]))*weight).sum())
 
 
-# 加权平均
-# OI = (np.array(VB) - np.array(VA))
-# dp_var = (data['bp1'][1:] +data['ap1'][1:])/2
-# dp_var_mov = []
-# n = 20
-# for i in range(n,len(dp_var)):
-#     dp_var_mov.append(dp_var[i-n:i-1].mean())
-#
-# for i in range(0,5):
-#     regr = linear_model.LinearRegression(normalize = True,copy_X = True)
-#     y = np.array(dp_var_mov).reshape(-1, 1)
-#     x = OI[n - i:len(OI) - i].reshape(-1, 1)
-#     regr.fit(x, y)
-#     print('lag='+ str(i)+'--R2='+ str(regr.score(x,y,sample_weight=None))+ '--' +'coef=' + str(regr.coef_[0][0]))
-
-
-
-#不加权平均
 OI = (np.array(VB) - np.array(VA))
 OI = np.insert(OI, 0, 0)
 dp_var = (data['bp1'][0:] +data['ap1'][0:])/2
@@ -53,20 +44,21 @@ dp_var = np.ediff1d(dp_var)
 dp_var = np.insert(dp_var, 0, 0)
 
 n = 5
-data['OI'] = pd.Series(OI)
+data['OI'] = OI
 data['OI1'] = data['OI'].shift(1)
 data['OI2'] = data['OI'].shift(2)
 data['OI3'] = data['OI'].shift(3)
 data['OI4'] = data['OI'].shift(4)
 data['OI5'] = data['OI'].shift(5)
 data['OI6'] = data['OI'].shift(6)
-data['midDiff'] = pd.Series(dp_var)
-data['midDiffMa'] = data.midDiff.rolling(5).mean()
+data['midDiff'] = dp_var
+data['midDiffMa'] = pd.rolling_mean(data.midDiff, 6)
 data = data.fillna(0)
+data.to_csv(filename, index=False)
 
 regr = linear_model.LinearRegression(normalize = True,copy_X = True)
 y = np.array(data['midDiffMa']).reshape(-1, 1)
 x = np.array(data[['OI', 'OI1', 'OI2', 'OI3', 'OI4', 'OI5']])
     #x = OI[n - i:len(OI) - i].reshape(-1, 1)
 regr.fit(x, y)
-print('--R2='+ str(regr.score(x,y,sample_weight=None))+ '--' +'coef=' + str(regr.coef_[0][0]))
+print('--R2='+ str(regr.score(x,y,sample_weight=None))+ '--coef=' + str(regr.coef_[0]) + '--intercept='+str(regr.intercept_[0]))
--- voi.py
+++ voi.py
@@ -1,21 +1,41 @@
 import pandas as pd
+import sys
+from datetime import datetime
 
-qty = 0
-remainingValue = 100000
-def order(depth, value, side):
-    if (side == -1 and remainingValue != 0) or (side == 1 and qty != 0):
-        return {'remainingValue': 0, 'qty': remainingValue / depth['ap1'] if side == -1 else remainingValue / depth['bp1']}
+filename = sys.argv[1]
+roundtime = int(sys.argv[2])
+res = pd.read_csv(filename)
+res['time'] = pd.Series(datetime.fromtimestamp(x) for x in res['timestamp'])
+res['time'] = pd.Series(x.replace(second = x.second/roundtime *roundtime, microsecond=0) for x in res['time'])
+res = res.set_index('time')
+res = res.groupby(level=0).first()
+
+qty = 0.0
+remainingValue = 100000.0
+transaction = 0
+costM = 1
+def order(tick, side):
+    print({'diff': tick['midDiffMa'], 'side': side, 'qty': qty, 'remainingValue': remainingValue})
+    if (side == -1 and remainingValue != 0):
+        return {'remainingValue': 0.0, 'qty': remainingValue / tick['ap1'] * costM}
+    if (side == 1 and qty != 0):
+        return {'remainingValue': qty * tick['bp1'] * costM, 'qty': 0}
+    return {'remainingValue': remainingValue , 'qty': qty}
 
 def getPriceDiff(idx):
     return coef['b0'] + coef['b1'] * res.iloc[idx]['voi'] + coef['b2'] * res.iloc[idx - 1]['voi'] + coef['b3'] * res.iloc[idx - 2]['voi'] + coef['b4'] * res.iloc[idx - 3]['voi'] + coef['b5'] * res.iloc[idx - 4]['voi'] + coef['b6'] * res.iloc[idx - 5]['voi']
         
 if __name__ == "__main__":
-    res = pd.read('btc-depth-20180827')
-    res['voi'] = [getVoi(x) for x in res]
-    for idx in [5 + x for x in range(len(res) - 5)]:
-        diff = getPriceDiff(idx)
-        if abs(diff) > 0.2:
-            pos = order(remainingValue, 1 if diff > 0.2 else -1) if remainingValue > 0
+    for index,tick in res.iterrows():
+        #print(abs(tick['midDiffMa']))
+        #0.00059361 * res['OI'] + 0.00061418 * res['OI1'] + 0.00065906 * res['OI2'] + 0.00064134 * res['OI3'] + 0.00073954 * res['OI4'] + 0.00013306 * res['OI5'] + 0.0108149578065
+        #diff = 0.233 * tick['OI'] + 0.342 * tick['OI1'] + 0.365 * tick['OI2'] + 0.389 * tick['OI3'] + 0.336 * tick['OI4'] + 0.494 * tick['OI5'] + 0.0202803706875
+        diff = 0.00453 * tick['OI'] + 0.0106 * tick['OI1'] + 0.0191 * tick['OI2'] + 0.0329 * tick['OI3'] + 0.0295 * tick['OI4'] + 0.0193 * tick['OI5'] + 0.312
+        if abs(tick['midDiffMa']) > 0.1:
+            pos = order(tick, -1 if tick['midDiffMa']> 0.25 else 1)
+            transaction = transaction + 1
+            print(pos)
             qty = pos['qty']
-            remainingValue = pos['qty']
-    print(remainingValue)
+            remainingValue = pos['remainingValue']
+    print(((remainingValue + qty * res.iloc[-1]['bp1'])/100000 - 1) * 100)
+    print(transaction)
