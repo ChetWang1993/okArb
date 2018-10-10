d1383172694303caae583ddf935a97e30d2018d7
ChetWang1993 <elwinwang1993@gmail.com>
2018-10-10 22:52:17 +0800

    crawl kline from binance

--- klineCrawler.py
+++ klineCrawler.py
@@ -1,7 +1,7 @@
-# encoding: UTF-8
-
+# --*-- encoding: utf-8 --*--
 import time
 import datetime
+from datetime import datetime
 from func import *
 
 # 将时间戳转化为标准时间
@@ -11,10 +11,17 @@ def to_localtime(strap):
     return dt
 
 
-symbol = "btg_usd"
+symbol = "BTGUSDT"
+start = "17 Jul, 2018"       # 开始时间
+end = "17 Sep, 2018"        # 结束时间
+interval = Client.KLINE_INTERVAL_1MINUTE        # K线的时间间隔
 
-startDate = datetime.datetime(2018,7,17)
-klines = getSpotKline(symbol, time.mktime(startDate.timetuple()) * 1000, '1min')      # 获取 K 线
+APIkey = 'R0546VwwTnhBNXdxi9a9Z7dkRHnCP8DyY0ah8KTDClxZqEOBaFkKgYLTLF8Acow8'
+Secretkey = 'J72JCQIxm3RRFDIwFXNWnSlmgKadEqaz184j2sjSeBGLBu9dDZ7kB7ImPR6Jdgqx'
+client = Client(APIkey, Secretkey)
+
+startDate = datetime(2018,7,17)
+klines = getFutureKline(symbol, datetime.time.mktime(startDate.timetuple()) * 1000)      # 获取 K 线
 # strap = klines[0][0]/1000		# 币安时间戳除以1000可获得正确时间戳
 
 # 比对官网数据，发现返回值前六项的含义以及对应顺序：时间戳，开，高，低，收，量
